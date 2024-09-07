b3c486d616e56814d407eaa4a791d8d085f57c24
Matheus Farias <matheusmlf@gmail.com>
2024-09-07 10:33:01 -0300

    implement:rotas crud

--- backend/login/v1/routes.py
+++ backend/login/v1/routes.py
@@ -2,9 +2,9 @@
 
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from login.v1.schemas import UserDB, UserPublic, UserSchema
+from login.v1.schemas import UserDB, UserList, UserPublic, UserSchema
 
 router = APIRouter()
 
@@ -23,3 +23,30 @@ def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
     return user_with_id
+
+
+@router.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@router.post(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@router.delete('/users/{user_id}', status_code=HTTPStatus.NO_CONTENT)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    database.pop(user_id - 1)
--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -32,3 +32,46 @@ def test_create_user():
         'email': 'alice@example.com',
         'id': 1,
     }
+
+
+def test_get_users():
+    client = TestClient(app)
+
+    response = client.get('login/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user():
+    client = TestClient(app)
+
+    response = client.post(
+        'login/users/1',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user():
+    client = TestClient(app)
+
+    response = client.delete('login/users/1')
+    assert response.status_code == HTTPStatus.NO_CONTENT
